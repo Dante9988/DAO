b557fee4ef149c3ba653b9f4dff6a58169add5d5
Dante9988 <xadja35@gmail.com>
2024-10-05 23:36:25 -0700

    Adds finalize and vote handlers

--- src/components/App.js
+++ src/components/App.js
@@ -4,33 +4,62 @@ import { ethers } from 'ethers'
 
 // Components
 import Navigation from './Navigation';
+import Create from './Create';
 import Loading from './Loading';
+import Proposals from './Proposals';
 
 // ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
+import DAO_ABI from '../abis/DAO.json'
 
 // Config: Import your network config here
-// import config from '../config.json';
+import config from '../config.json';
 
 function App() {
   const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null)
+  const [proposals, setProposals] = useState(null)
+  const [dao, setDao] = useState(null)
+  const [treasuryBalance, setTreasuryBalance] = useState(0)
   const [balance, setBalance] = useState(0)
-
+  const [quorum, setQuorum] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
+
+  let block;
 
   const loadBlockchainData = async () => {
     // Initiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+    const { chainId } = await provider.getNetwork()
+    block = await provider.getBlockNumber()
+
+    // Initiate contracts
+    const dao = new ethers.Contract(config[chainId].dao.address, DAO_ABI, provider)
+    setDao(dao);
+    
+    setTreasuryBalance(
+      ethers.utils.formatUnits(
+        await provider.getBalance(dao.address), 18
+      )
+    );
 
     // Fetch accounts
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
     const account = ethers.utils.getAddress(accounts[0])
     setAccount(account)
 
-    // Fetch account balance
-    let balance = await provider.getBalance(account)
-    balance = ethers.utils.formatUnits(balance, 18)
-    setBalance(balance)
+    const count = await dao.proposalCount()
+
+    const items = []
+    for (let i = 0; i < count; i++) {
+      // fetch proposals
+      const proposal = await dao.proposals(i + 1)
+      items.push(proposal)
+    }
+
+    setProposals(items);
+
+    setQuorum(await dao.quorum())
 
     setIsLoading(false)
   }
@@ -45,14 +74,20 @@ function App() {
     <Container>
       <Navigation account={account} />
 
-      <h1 className='my-4 text-center'>React Hardhat Template</h1>
+      <h1 className='my-4 text-center'>Welcome to Dragon DAO!</h1>
 
       {isLoading ? (
         <Loading />
       ) : (
         <>
-          <p className='text-center'><strong>Your ETH Balance:</strong> {balance} ETH</p>
-          <p className='text-center'>Edit App.js to add your code here.</p>
+          <Create provider={provider} dao={dao} setIsLoading={setIsLoading}/>
+          <hr/>
+
+          <p className='text-center'><strong>Treasury Balance:</strong> {treasuryBalance} ETH</p>
+
+          <hr/>
+
+          <Proposals provider={provider} dao={dao} proposals={proposals} quorum={quorum} setIsLoading={setIsLoading}/>
         </>
       )}
     </Container>
---
+++ src/components/Create.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Spinner from 'react-bootstrap/Spinner'
+import { ethers } from 'ethers'
+
+const Create = ({ provider, dao, setIsLoading }) => {
+    return (
+        <p>Create form here...</p>
+    )
+}
+
+export default Create;
